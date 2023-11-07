a1d785c13e9a5a7a8b7946443b689a1a86b95133
alkjf <alkjfgh@naver.com>
2023-11-07 15:18:12 +0900

    코드 정리 및 주석 추가

--- servers/server.js
+++ servers/server.js
@@ -3,12 +3,12 @@ const app = express();
 // const cors = require('cors');
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3001;
-const route = require('./routes/index');
+const route = require('./routes/board');
 
 // app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/api', route); // app.use('/api', (req, res)=> res.json({username:'bryan'}));
+app.use('/api', route);
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
