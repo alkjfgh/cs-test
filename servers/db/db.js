bab7f0e3426aef28643949716bfb041f12c69d9a
alkjf <alkjfgh@naver.com>
2023-11-07 15:13:25 +0900

    코드 정리 및 주석 추가

--- servers/db/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-const config = require('../db/db_config.json');
-
-async function getConnection() {
-    const pool = await mysql.createPool(config);
-    return pool.getConnection();
-}
-
-module.exports = getConnection;
